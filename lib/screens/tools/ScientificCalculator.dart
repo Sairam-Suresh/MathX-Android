8e359403de565558bc769c1dd9f0388c29ca1e1e
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-03-22 22:55:10 +0800

    Implemented Display of the scientific calculator

--- lib/screens/tools/ScientificCalculator.dart
+++ lib/screens/tools/ScientificCalculator.dart
@@ -11,6 +11,41 @@ class ScientificCalculatorView extends StatefulWidget {
 class _ScientificCalculatorViewState extends State<ScientificCalculatorView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(title: const Text("Scientific Calculator")),
+        body: Column(children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.25,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.green),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(children: [
+                    Row(
+                      children: const [
+                        Text("0",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 30)),
+                        Spacer()
+                      ],
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: const [
+                        Spacer(),
+                        Text("0",
+                            style: TextStyle(color: Colors.white, fontSize: 30))
+                      ],
+                    )
+                  ]),
+                ),
+              ),
+            ),
+          )
+        ]));
   }
 }
