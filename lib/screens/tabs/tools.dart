7f67acc3963e925886fe24a84f1b979a265d9f2d
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-03-22 17:57:10 +0800

    Made base gridview for tools

--- lib/screens/tabs/tools.dart
+++ lib/screens/tabs/tools.dart
@@ -10,6 +10,70 @@ class ToolsPage extends StatefulWidget {
 class _ToolsPageState extends State<ToolsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("Tools")),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+          ToolTile(
+            color: Colors.red,
+            child: Text("hello"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ToolTile extends StatelessWidget {
+  ToolTile({super.key, required this.color, required this.child});
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)), color: color),
+      padding: const EdgeInsets.all(8),
+      child: child,
+    );
   }
 }
