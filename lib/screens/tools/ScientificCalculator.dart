cbaba5d6960670b01d5c9c0a9fe7870008b6db30
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-03-22 22:34:14 +0800

    Added boilerplate code for calculator

---
+++ lib/screens/tools/ScientificCalculator.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ScientificCalculatorView extends StatefulWidget {
+  const ScientificCalculatorView({Key? key}) : super(key: key);
+
+  @override
+  State<ScientificCalculatorView> createState() =>
+      _ScientificCalculatorViewState();
+}
+
+class _ScientificCalculatorViewState extends State<ScientificCalculatorView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
