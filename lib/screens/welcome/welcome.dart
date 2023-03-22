6ed7f9391c9623cf6bd2002e274bc3478328ee1b
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-03-22 10:33:10 +0800

    Built Basic Screen with text and color

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -10,6 +10,50 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: RadialGradient(
+                colors: [Colors.purpleAccent, Colors.white],
+                stops: [0.0, 1.0],
+                center: Alignment.topCenter,
+                radius: 1.5)),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(children: [
+              const Spacer(flex: 1),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: Column(
+                  children: const [
+                    Center(
+                        child: Text("MathX",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold))),
+                    Text("By AppCatalyst Inc",
+                        style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+              const Flexible(
+                  fit: FlexFit.tight,
+                  flex: 4,
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                        "Disclaimer: Please only use this app at the appropriate times and when necessary.",
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                  ))
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
