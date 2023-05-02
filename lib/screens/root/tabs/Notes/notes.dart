47465dc9048999a90fce840e417a88ceeb6cb6a9
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-02 15:46:36 +0800

    Made Notes page with notes card for note previews

--- lib/screens/root/tabs/Notes/notes.dart
+++ lib/screens/root/tabs/Notes/notes.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mathx_android/constants.dart';
+import 'package:mathx_android/widgets/notecard.dart';
 
 class NotesPage extends StatelessWidget {
   const NotesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        padding: EdgeInsets.all(PADDING_BETWEEN_SQUARES),
+        children: [NoteCard(name: "Note 1")],
+      ),
+    );
   }
 }
