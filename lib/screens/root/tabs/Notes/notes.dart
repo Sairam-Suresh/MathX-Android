47c4eb3f9a4475f98c611766609aef01aa781ade
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-04 12:32:27 +0800

    Extracted data in page to a separate variable

--- lib/screens/root/tabs/Notes/notes.dart
+++ lib/screens/root/tabs/Notes/notes.dart
@@ -2,11 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:mathx_android/constants.dart';
 import 'package:mathx_android/widgets/notecard.dart';
 
-class NotesPage extends StatelessWidget {
+class NotesPage extends StatefulWidget {
   const NotesPage({Key? key}) : super(key: key);
 
   @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  List<Note> listOfNotes = [
+    Note(
+        name: "Note 1",
+        description: "Description 1",
+        date: DateTime.now(),
+        content: "Content 1"),
+    Note(
+        name: "Note 2",
+        description: "Description 2",
+        date: DateTime.now(),
+        content: "Content 2")
+  ];
+
+  List<Widget> noteCards = [];
+
+  @override
   Widget build(BuildContext context) {
+    for (Note note in listOfNotes) {
+      noteCards.add(NoteCard(name: note.name));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Notes"),
@@ -20,7 +44,7 @@ class NotesPage extends StatelessWidget {
           crossAxisCount: 2,
         ),
         padding: EdgeInsets.all(PADDING_BETWEEN_SQUARES),
-        children: [NoteCard(name: "Note 1"), NoteCard(name: "Note 1")],
+        children: noteCards,
       ),
     );
   }
