1829401c3003627e099a81d4ffdbcb1ebc33b567
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-04 16:27:51 +0800

    Added superbasic scaffold

--- lib/screens/root/tabs/Cheatsheet/cheatsheet.dart
+++ lib/screens/root/tabs/Cheatsheet/cheatsheet.dart
@@ -5,6 +5,11 @@ class CheatsheetPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cheatsheets"),
+      ),
+      body: const Placeholder(),
+    );
   }
 }
