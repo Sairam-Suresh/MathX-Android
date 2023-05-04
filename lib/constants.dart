27d8d8f9d743bd958e965baf15df27969d1ec823
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-04 12:32:07 +0800

    Added Note structure

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,14 @@
 // Tools View
 double PADDING_BETWEEN_SQUARES = 10;
+
+class Note {
+  Note(
+      {required this.name,
+      required this.description,
+      required this.date,
+      required this.content});
+  late String name;
+  late String description;
+  late DateTime date;
+  late String content;
+}
