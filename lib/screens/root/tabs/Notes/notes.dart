b597558f8d148d9ba39ab96374a232b98518d407
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-04 16:03:29 +0800

    Fixed a bug in the create new note view where the show date picker button would not update if a date was selected

--- lib/screens/root/tabs/Notes/notes.dart
+++ lib/screens/root/tabs/Notes/notes.dart
@@ -39,30 +39,6 @@ class _NotesPageState extends State<NotesPage> {
     String desc = "";
     DateTime? time;
 
-    _selectDate(BuildContext context) async {
-      final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(), // Refer step 1
-        firstDate: DateTime(2000),
-        lastDate: DateTime(2025),
-      );
-      if (picked != null) {
-        setState(() {
-          time = picked;
-        });
-        return picked;
-      }
-    }
-
-    void rebuildAllChildren(BuildContext context) {
-      void rebuild(Element el) {
-        el.markNeedsBuild();
-        el.visitChildren(rebuild);
-      }
-
-      (context as Element).visitChildren(rebuild);
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notes"),
@@ -74,95 +50,102 @@ class _NotesPageState extends State<NotesPage> {
             builder: (BuildContext context) {
               return BottomSheet(
                 builder: (BuildContext context) {
-                  return Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Create New Note',
-                          textAlign: TextAlign.start,
-                          style: TextStyle(fontSize: 30),
-                        ),
-                        const Spacer(),
-                        TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: "Note Name",
-                            ),
-                            onChanged: (value) {
-                              setState(() {
-                                name = value;
-                              });
-                            }),
-                        const Spacer(),
-                        TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              hintText: "Description",
-                            ),
-                            onChanged: (value) {
-                              setState(() {
-                                desc = value;
-                              });
-                            }),
-                        const Spacer(),
-                        Center(
-                          child: FilledButton(
-                            onPressed: () async {
-                              time = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(), // Refer step 1
-                                firstDate: DateTime(2000),
-                                lastDate: DateTime(2025),
-                              );
+                  return StatefulBuilder(
+                      builder: (BuildContext context, StateSetter setState) {
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Create New Note',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          const Spacer(),
+                          TextField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Note Name",
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  name = value;
+                                });
+                              }),
+                          const Spacer(),
+                          TextField(
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Description",
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  desc = value;
+                                });
+                              }),
+                          const Spacer(),
+                          Center(
+                            child: FilledButton(
+                              onPressed: () async {
+                                time = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(), // Refer step 1
+                                  firstDate: DateTime(2000),
+                                  lastDate: DateTime(2025),
+                                );
 
-                              setState(() {});
-                              setState(() {});
-                              // _selectDate(context).then((value) {
-                              //   if (value != null) {
-                              //     setState(() {
-                              //       time = value;
-                              //     });
-                              //   }
-                              // }).whenomplete(() {
-                              //   setState(() {});
-                              // });
-                              // setState(() {
-                              //
-                              // });
-                              //
-                            },
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text("Select Date" + time.toString()),
-                                Icon(time != null ? Icons.check : Icons.close),
-                              ],
+                                setState(() {});
+                                setState(() {
+                                  time = time;
+                                });
+
+                                // _selectDate(context).then((value) {
+                                //   if (value != null) {
+                                //     setState(() {
+                                //       time = value;
+                                //     });
+                                //   }
+                                // }).whenomplete(() {
+                                //   setState(() {});
+                                // });
+                                // setState(() {
+                                //
+                                // });
+                                //
+                              },
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text("Select Date"),
+                                  Icon(
+                                      time != null ? Icons.check : Icons.close),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        const Spacer(flex: 6),
-                        Center(
-                          child: FilledButton(
-                            onPressed: () {
-                              setState(() {
-                                listOfNotes.add(Note(
-                                  name: name,
-                                  description: desc,
-                                  date: time!,
-                                  content: "",
-                                ));
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: const Text("Save"),
+                          const Spacer(flex: 6),
+                          Center(
+                            child: FilledButton(
+                              onPressed: () {
+                                setState(() {
+                                  listOfNotes.add(Note(
+                                    name: name,
+                                    description: desc,
+                                    date: time!,
+                                    content: "",
+                                  ));
+                                });
+                                Navigator.pop(context);
+                              },
+                              child: const Text("Save"),
+                            ),
                           ),
-                        ),
-                        const Spacer(flex: 2)
-                      ],
-                    ),
-                  );
+                          const Spacer(flex: 2)
+                        ],
+                      ),
+                    );
+                  });
                 },
                 onClosing: () {},
                 enableDrag: false,
