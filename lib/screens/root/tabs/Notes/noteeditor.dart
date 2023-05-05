c420c857d0d23a5ba1d4162f48237a4f036706a8
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-05 11:43:39 +0800

    Created noteeditor.dart to create notes

---
+++ lib/screens/root/tabs/Notes/noteeditor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteEditorPage extends StatefulWidget {
+  const NoteEditorPage({Key? key}) : super(key: key);
+
+  @override
+  _NoteEditorPageState createState() => _NoteEditorPageState();
+}
+
+class _NoteEditorPageState extends State<NoteEditorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
