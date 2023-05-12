0faf108eb71cd3c5dea15790979d391832027882
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-12 12:04:26 +0800

    Finished super basic implementation for multiple text fields

--- lib/screens/root/tabs/Tools/tools/HCFLCM.dart
+++ lib/screens/root/tabs/Tools/tools/HCFLCM.dart
@@ -1,13 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
-class HCFLCMPage extends StatelessWidget {
+class TextFieldData {
+  final String text;
+  final int index;
+
+  TextFieldData({required this.text, required this.index});
+}
+
+class HCFLCMPage extends StatefulWidget {
   const HCFLCMPage({Key? key}) : super(key: key);
+
+  @override
+  State<HCFLCMPage> createState() => _HCFLCMPageState();
+}
+
+class _HCFLCMPageState extends State<HCFLCMPage> {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+  List<String> textFields = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("HCF & LCM")),
-      body: Text("Hello world"),
+      appBar: AppBar(title: const Text("HCF & LCM")
+          //     , actions: [
+          //   IconButton(
+          //     icon: Icon(Icons.add),
+          //     onPressed: addTextField,
+          //   )
+          // ]
+          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: textFields.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: FormBuilderTextField(
+                        name: 'text_$index',
+                        onChanged: (value) {
+                          _formKey.currentState?.save();
+                          if (_formKey.currentState?.validate() == true) {}
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Text ${index + 1}',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              setState(() {
+                                textFields.removeAt(index);
+                              });
+                            },
+                          ),
+                        ),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(),
+                          FormBuilderValidators.numeric(
+                              errorText: 'Please enter a valid number'),
+                        ]),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState?.save();
+                  if (_formKey.currentState?.validate() == true) {
+                    setState(() {
+                      textFields.add('');
+                    });
+                  }
+                },
+                child: Text('Add TextField'),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState?.save();
+                  if (_formKey.currentState?.validate() == true) {
+                    // Retrieve all form field values
+                    Map<String, dynamic> formValues =
+                        _formKey.currentState?.value ?? {};
+                    // Process the form data as needed
+                    print(formValues);
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
