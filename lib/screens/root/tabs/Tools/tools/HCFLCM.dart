1d88b25fd1e2149a7106de51ba6825afee9a6436
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-13 17:59:59 +0800

    Full HCF LCM calculator implemented with no bugs

--- lib/screens/root/tabs/Tools/tools/HCFLCM.dart
+++ lib/screens/root/tabs/Tools/tools/HCFLCM.dart
@@ -1,18 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:mathx_android/constants.dart';
 import 'package:mathx_android/logic/tools/HCFLCMCalculator.dart';
-
-class TextFieldData {
-  final String text;
-  final int index;
-
-  TextFieldData({required this.text, required this.index});
-}
 
 class HCFLCMPage extends StatefulWidget {
   const HCFLCMPage({Key? key}) : super(key: key);
@@ -22,11 +13,24 @@ class HCFLCMPage extends StatefulWidget {
 }
 
 class _HCFLCMPageState extends State<HCFLCMPage> {
-  GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
-  List<String> textFields = [];
-  Map<String, dynamic> formValues = {};
-  List<int> values = [];
   HCFLCM selectedMode = HCFLCM.HCF;
+
+  final _formKey = GlobalKey<FormBuilderState>();
+  List<String> textFieldValues = [];
+
+  void addTextField() {
+    setState(() {
+      textFieldValues.add('');
+    });
+  }
+
+  void deleteTextField() {
+    setState(() {
+      if (textFieldValues.isNotEmpty) {
+        textFieldValues.removeLast();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,13 +57,21 @@ class _HCFLCMPageState extends State<HCFLCMPage> {
                                 style: const TextStyle(fontSize: 25),
                               ),
                               Text(
-                                (_formKey.currentState?.isValid ?? false) &&
-                                        values.isNotEmpty &&
-                                        textFields.length >= 2
+                                textFieldValues.length >= 2 &&
+                                        (_formKey.currentState?.isValid ??
+                                            false)
                                     ? selectedMode == HCFLCM.HCF
-                                        ? calculateHCFForMultiple(values)
+                                        ? calculateHCFForMultiple(
+                                                textFieldValues
+                                                    .map((e) =>
+                                                        int.tryParse(e) ?? 0)
+                                                    .toList())
                                             .toString()
-                                        : calculateLCMForMultiple(values)
+                                        : calculateLCMForMultiple(
+                                                textFieldValues
+                                                    .map((e) =>
+                                                        int.tryParse(e) ?? 0)
+                                                    .toList())
                                             .toString()
                                     : "--",
                                 style: const TextStyle(fontSize: 40),
@@ -70,33 +82,12 @@ class _HCFLCMPageState extends State<HCFLCMPage> {
                 }),
         appBar: AppBar(title: const Text("HCF & LCM"), actions: [
           IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              setState(() {
-                _formKey.currentState?.reset();
-                _formKey.currentState?.save();
-
-                values.clear();
-                textFields.clear();
-                formValues.clear();
-                values.clear();
-              });
-            },
+            icon: const Icon(Icons.undo),
+            onPressed: deleteTextField,
           ),
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {
-              _formKey.currentState?.save();
-              if (_formKey.currentState?.validate() == true) {
-                setState(() {
-                  textFields.add('');
-                  formValues = _formKey.currentState?.value ?? {};
-                  values =
-                      formValues.values.map((e) => int.parse(e))?.toList() ??
-                          [];
-                });
-              }
-            },
+            onPressed: addTextField,
           ),
         ]),
         body: KeyboardVisibilityBuilder(
@@ -111,78 +102,63 @@ class _HCFLCMPageState extends State<HCFLCMPage> {
                         MediaQuery.of(context).viewInsets.bottom,
                 left: 16.0,
                 right: 16.0),
-            child: FormBuilder(
-              key: _formKey,
-              onChanged: () {
-                if (_formKey.currentState?.validate() == true) {
-                  formValues =
-                      json.decode(json.encode(_formKey.currentState?.value)) ??
-                          {};
-                  values = formValues.values.map((e) => int.parse(e)).toList();
-
-                  // print("-------------------------------");
-                  // print(_formKey.currentState?.value);
-                  // print("-------------------------------");
-                  setState(() {});
-                }
-              },
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.height,
-                      child: SegmentedButton(
-                        segments: const [
-                          ButtonSegment(value: HCFLCM.HCF, label: Text("HCF")),
-                          ButtonSegment(value: HCFLCM.LCM, label: Text("LCM")),
-                        ],
-                        selected: {selectedMode},
-                        onSelectionChanged: (Set<HCFLCM> newSelection) {
-                          setState(() {
-                            selectedMode = newSelection.first;
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: textFields.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          child: FormBuilderTextField(
-                            name: 'text_$index',
-                            autovalidateMode:
-                                AutovalidateMode.onUserInteraction,
-                            onChanged: (value) {
-                              _formKey.currentState?.save();
-                              formValues = json.decode(json
-                                      .encode(_formKey.currentState?.value)) ??
-                                  {};
-                              values = formValues.values
-                                  .map((e) => int.parse(e))
-                                  .toList();
-
-                              print(formValues);
-                              print(values);
-                              setState(() {});
-                            },
-                            decoration: InputDecoration(
-                                labelText: 'Number ${index + 1}'),
-                            validator: FormBuilderValidators.compose([
-                              FormBuilderValidators.required(),
-                              FormBuilderValidators.numeric(
-                                  errorText: 'Please enter a valid number'),
-                            ]),
-                          ),
-                        );
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.height,
+                    child: SegmentedButton(
+                      segments: const [
+                        ButtonSegment(value: HCFLCM.HCF, label: Text("HCF")),
+                        ButtonSegment(value: HCFLCM.LCM, label: Text("LCM")),
+                      ],
+                      selected: {selectedMode},
+                      onSelectionChanged: (Set<HCFLCM> newSelection) {
+                        setState(() {
+                          selectedMode = newSelection.first;
+                        });
                       },
                     ),
                   ),
-                ],
-              ),
+                ),
+                FormBuilder(
+                  key: _formKey,
+                  clearValueOnUnregister: true,
+                  onChanged: () {
+                    print(textFieldValues);
+                  },
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        itemCount: textFieldValues.length,
+                        shrinkWrap: true,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 10),
+                            child: FormBuilderTextField(
+                              name: 'number_${index + 1}',
+                              autovalidateMode: AutovalidateMode.always,
+                              validator: FormBuilderValidators.compose([
+                                FormBuilderValidators.numeric(),
+                                FormBuilderValidators.required()
+                              ]),
+                              decoration: InputDecoration(
+                                labelText: 'Number ${index + 1}',
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  textFieldValues[index] = value ?? "";
+                                });
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           );
         }),
