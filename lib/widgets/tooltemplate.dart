b9a152a70e56fdf72b31637e2d30fb003330f256
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-16 12:29:57 +0800

    Added bottomsheet hide on keyboard appearance

--- lib/widgets/tooltemplate.dart
+++ lib/widgets/tooltemplate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
 class Tooltemplate extends StatefulWidget {
@@ -79,106 +80,117 @@ class _TooltemplateState extends State<Tooltemplate> {
       ],
     );
 
-    return Scaffold(
-      key: _scaffoldKey,
-      bottomSheet: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: BottomSheet(
-            onClosing: () {},
-            builder: (BuildContext context) {
-              return SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 15, left: 15, right: 15, bottom: 30),
-                      child: widget.bottomSheetContent(
-                          textFieldValues, selectedValues, _formKey)));
-            }),
-      ),
-      appBar: generatedAppBar,
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            widget.segmentedButtonMultiSelect != null
-                ? Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.height,
-                      child: SegmentedButton(
-                        segments: widget.options.entries
-                            .map((e) => ButtonSegment(
-                                value: e.value, label: Text(e.key)))
-                            .toList(),
-                        selected: selectedValues,
-                        multiSelectionEnabled:
-                            widget.segmentedButtonMultiSelect!,
-                        onSelectionChanged: (dynamic newSelection) {
-                          setState(() {
-                            selectedValues = newSelection;
-                            print(selectedValues);
-                          });
-                        },
-                      ),
-                    ),
-                  )
-                : Container(),
-            FormBuilder(
-              key: _formKey,
-              clearValueOnUnregister: true,
-              onChanged: () {
-                print(textFieldValues);
-              },
-              child: Expanded(
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: textFieldValues.length,
-                        shrinkWrap: true,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
-                            padding: const EdgeInsets.only(bottom: 10),
-                            child: FormBuilderTextField(
-                              name: 'number_${index + 1}',
-                              autovalidateMode: AutovalidateMode.always,
-                              validator: FormBuilderValidators.compose([
-                                FormBuilderValidators.numeric(),
-                                FormBuilderValidators.required()
-                              ]),
-                              decoration: InputDecoration(
-                                labelText: 'Number ${index + 1}',
-                              ),
-                              onChanged: (value) {
-                                setState(() {
-                                  textFieldValues[index] = value ?? "";
-                                });
-                              },
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-
-                    // This container is FAKE. It will HIDE BEHIND the main bottom sheet. It will only
-                    // Act as a way to push the scrollview up so that it does not hide behind the bottomsheet
-                    Container(
-                      color: Colors.transparent,
-                      child: SizedBox(
+    return KeyboardVisibilityBuilder(builder: (context, isKeyboardVisible) {
+      return Scaffold(
+        key: _scaffoldKey,
+        bottomSheet: !isKeyboardVisible
+            ? SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: BottomSheet(
+                    onClosing: () {},
+                    builder: (BuildContext context) {
+                      return SizedBox(
                           width: MediaQuery.of(context).size.width,
                           child: Padding(
                               padding: const EdgeInsets.only(
-                                  top: 0, left: 15, right: 15, bottom: 30),
+                                  top: 15, left: 15, right: 15, bottom: 30),
                               child: widget.bottomSheetContent(
-                                  textFieldValues, selectedValues, _formKey))),
-                    ),
-                  ],
+                                  textFieldValues, selectedValues, _formKey)));
+                    }),
+              )
+            : null,
+        appBar: generatedAppBar,
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              widget.segmentedButtonMultiSelect != null
+                  ? Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.height,
+                        child: SegmentedButton(
+                          segments: widget.options.entries
+                              .map((e) => ButtonSegment(
+                                  value: e.value, label: Text(e.key)))
+                              .toList(),
+                          selected: selectedValues,
+                          multiSelectionEnabled:
+                              widget.segmentedButtonMultiSelect!,
+                          onSelectionChanged: (dynamic newSelection) {
+                            setState(() {
+                              selectedValues = newSelection;
+                              print(selectedValues);
+                            });
+                          },
+                        ),
+                      ),
+                    )
+                  : Container(),
+              FormBuilder(
+                key: _formKey,
+                clearValueOnUnregister: true,
+                onChanged: () {
+                  print(textFieldValues);
+                },
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: textFieldValues.length,
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: FormBuilderTextField(
+                                name: 'number_${index + 1}',
+                                autovalidateMode: AutovalidateMode.always,
+                                validator: FormBuilderValidators.compose([
+                                  FormBuilderValidators.numeric(),
+                                  FormBuilderValidators.required()
+                                ]),
+                                decoration: InputDecoration(
+                                  labelText: 'Number ${index + 1}',
+                                ),
+                                onChanged: (value) {
+                                  setState(() {
+                                    textFieldValues[index] = value ?? "";
+                                  });
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+
+                      // This container is FAKE. It will HIDE BEHIND the main bottom sheet. It will only
+                      // Act as a way to push the scrollview up so that it does not hide behind the bottomsheet
+                      !isKeyboardVisible
+                          ? Container(
+                              color: Colors.transparent,
+                              child: SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  child: Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 0,
+                                          left: 15,
+                                          right: 15,
+                                          bottom: 30),
+                                      child: widget.bottomSheetContent(
+                                          textFieldValues,
+                                          selectedValues,
+                                          _formKey))),
+                            )
+                          : Container(),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
