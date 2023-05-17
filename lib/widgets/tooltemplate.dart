01f9f3589dcdfcb13b404c003691e0d1cbdc363d
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-17 17:31:05 +0800

    Many Enhancements

--- lib/widgets/tooltemplate.dart
+++ lib/widgets/tooltemplate.dart
@@ -9,20 +9,31 @@ class Tool extends StatefulWidget {
       required this.appbar,
       this.segmentedButtonMultiSelect,
       required this.options,
-      required this.bottomSheetContent,
+      required this.resultContent,
       this.limitEntries,
-      this.validatorComposer})
+      this.validatorComposer,
+      this.floatingActionButton,
+      this.floatingActionButtonLocation,
+      this.bottomNavigationBar,
+      this.child})
       : super(key: key);
 
   // Generic Arguments
   final bool? segmentedButtonMultiSelect;
+
   final Map<String, dynamic>? options;
+
   final Function(List<String> list, Set<dynamic>? selectedValues,
-      GlobalKey<FormBuilderState> formKey) bottomSheetContent;
+      GlobalKey<FormBuilderState> formKey) resultContent;
   final AppBar appbar;
 
   final List? limitEntries;
   final dynamic validatorComposer;
+
+  final Widget? child;
+  final Widget? floatingActionButton;
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+  final Widget? bottomNavigationBar;
 
   @override
   _ToolState createState() => _ToolState();
@@ -79,142 +90,190 @@ class _ToolState extends State<Tool> {
   Widget build(BuildContext context) {
     generatedAppBar = AppBar(
       title: widget.appbar.title,
-      actions: widget.limitEntries == null
-          ? [
-              IconButton(
-                icon: const Icon(Icons.undo),
-                onPressed: deleteTextField,
-              ),
-              IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: addTextField,
-              ),
-            ]
-          : null,
+      automaticallyImplyLeading: false,
     );
 
     return KeyboardVisibilityBuilder(builder: (context, isKeyboardVisible) {
-      return Scaffold(
-        key: _scaffoldKey,
-        bottomSheet: !isKeyboardVisible
-            ? SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: BottomSheet(
-                    onClosing: () {},
-                    enableDrag: false,
-                    builder: (BuildContext context) {
-                      return SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 30, left: 30, right: 30, bottom: 30),
-                              child: widget.bottomSheetContent(
-                                  textFieldValues, selectedValues, _formKey)));
-                    }),
-              )
-            : null,
-        appBar: generatedAppBar,
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: () {},
-        //   child: Icon(Icons.add),
-        // ),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              widget.segmentedButtonMultiSelect != null
-                  ? Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.height,
-                        child: SegmentedButton(
-                          segments: widget.options!.entries
-                              .map((e) => ButtonSegment(
-                                  value: e.value, label: Text(e.key)))
-                              .toList(),
-                          selected: selectedValues,
-                          multiSelectionEnabled:
-                              widget.segmentedButtonMultiSelect!,
-                          onSelectionChanged: (dynamic newSelection) {
-                            setState(() {
-                              selectedValues = newSelection;
-                              print(selectedValues);
-                            });
-                          },
-                        ),
-                      ),
+      return GestureDetector(
+        onTap: () {
+          final currentFocus = FocusScope.of(context);
+          if (!currentFocus.hasPrimaryFocus && currentFocus.hasFocus) {
+            FocusManager.instance.primaryFocus?.unfocus();
+          }
+        },
+        child: Scaffold(
+          key: _scaffoldKey,
+          appBar: generatedAppBar,
+          floatingActionButton: widget.floatingActionButton == null
+              ? widget.limitEntries == null && widget.child == null
+                  ? FloatingActionButton(
+                      onPressed: () {
+                        addTextField();
+                      },
+                      child: const Icon(Icons.add),
                     )
-                  : Container(),
-              FormBuilder(
-                key: _formKey,
-                clearValueOnUnregister: true,
-                onChanged: () {
-                  print(textFieldValues);
-                },
-                child: Expanded(
+                  : null
+              : widget.floatingActionButton!,
+          floatingActionButtonLocation:
+              widget.floatingActionButtonLocation == null
+                  ? FloatingActionButtonLocation.endContained
+                  : widget.floatingActionButtonLocation!,
+          bottomNavigationBar: widget.bottomNavigationBar == null
+              ? BottomAppBar(
+                  child: widget.limitEntries == null && widget.child == null
+                      ? Row(
+                          children: [
+                            Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(Icons.chevron_left),
+                                      Text("Back")
+                                    ],
+                                  ),
+                                )),
+                            Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                child: ElevatedButton(
+                                  onPressed: textFieldValues.length != 0
+                                      ? () {
+                                          setState(() {
+                                            textFieldValues.clear();
+                                          });
+                                        }
+                                      : null,
+                                  child: const Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(Icons.delete_forever),
+                                      Text("Clear Values")
+                                    ],
+                                  ),
+                                )),
+                          ],
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.chevron_left),
+                                Text("Back")
+                              ],
+                            ),
+                          )),
+                )
+              : widget.bottomNavigationBar!,
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {},
+          //   child: Icon(Icons.add),
+          // ),
+          body: widget.child == null
+              ? Padding(
+                  padding: const EdgeInsets.all(10),
                   child: Column(
                     children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: widget.limitEntries == null
-                              ? textFieldValues.length
-                              : widget.limitEntries!.length,
-                          shrinkWrap: true,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(bottom: 10),
-                              child: FormBuilderTextField(
-                                name: widget.limitEntries == null
-                                    ? 'number_${index + 1}'
-                                    : widget.limitEntries![index],
-                                autovalidateMode: AutovalidateMode.always,
-                                validator: widget.validatorComposer != null
-                                    ? widget.validatorComposer!
-                                    : FormBuilderValidators.compose([
-                                        FormBuilderValidators.numeric(),
-                                        FormBuilderValidators.required()
-                                      ]),
-                                decoration: InputDecoration(
-                                  labelText: widget.limitEntries == null
-                                      ? 'Number ${index + 1}'
+                      Card(
+                        child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 10),
+                                child: widget.resultContent(textFieldValues,
+                                    selectedValues, _formKey))),
+                      ),
+                      FormBuilder(
+                        key: _formKey,
+                        clearValueOnUnregister: true,
+                        onChanged: () {
+                          print(textFieldValues);
+                        },
+                        child: Expanded(
+                          child: ListView.builder(
+                            itemCount: widget.limitEntries == null
+                                ? textFieldValues.length
+                                : widget.limitEntries!.length,
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: FormBuilderTextField(
+                                  name: widget.limitEntries == null
+                                      ? 'number_${index + 1}'
                                       : widget.limitEntries![index],
+                                  keyboardType: TextInputType.number,
+                                  autovalidateMode:
+                                      AutovalidateMode.onUserInteraction,
+                                  validator: widget.validatorComposer != null
+                                      ? widget.validatorComposer!
+                                      : FormBuilderValidators.compose([
+                                          FormBuilderValidators.numeric(),
+                                          FormBuilderValidators.required()
+                                        ]),
+                                  decoration: InputDecoration(
+                                      labelText: widget.limitEntries == null
+                                          ? 'Number ${index + 1}'
+                                          : widget.limitEntries![index],
+                                      suffixIcon: widget.limitEntries == null &&
+                                              index ==
+                                                  textFieldValues.length - 1
+                                          ? IconButton(
+                                              onPressed: () {
+                                                deleteTextField();
+                                              },
+                                              icon: Icon(Icons.delete),
+                                            )
+                                          : null),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      textFieldValues[index] = value ?? "";
+                                    });
+                                  },
                                 ),
-                                onChanged: (value) {
-                                  setState(() {
-                                    textFieldValues[index] = value ?? "";
-                                  });
-                                },
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
                       ),
-
-                      // This container is FAKE. It will HIDE BEHIND the main bottom sheet. It will only
-                      // Act as a way to push the scrollview up so that it does not hide behind the bottomsheet
-                      !isKeyboardVisible
-                          ? Container(
-                              color: Colors.transparent,
+                      widget.segmentedButtonMultiSelect != null
+                          ? Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
                               child: SizedBox(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 0,
-                                          left: 15,
-                                          right: 15,
-                                          bottom: 30),
-                                      child: widget.bottomSheetContent(
-                                          textFieldValues,
-                                          selectedValues,
-                                          _formKey))),
+                                width: MediaQuery.of(context).size.height,
+                                child: SegmentedButton(
+                                  segments: widget.options!.entries
+                                      .map((e) => ButtonSegment(
+                                          value: e.value, label: Text(e.key)))
+                                      .toList(),
+                                  selected: selectedValues,
+                                  multiSelectionEnabled:
+                                      widget.segmentedButtonMultiSelect!,
+                                  onSelectionChanged: (dynamic newSelection) {
+                                    setState(() {
+                                      selectedValues = newSelection;
+                                      print(selectedValues);
+                                    });
+                                  },
+                                ),
+                              ),
                             )
                           : Container(),
                     ],
                   ),
-                ),
-              ),
-            ],
-          ),
+                )
+              : widget.child!,
         ),
       );
     });
