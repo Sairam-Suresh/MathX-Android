ebb19b547b7fde9dee2b344ff02dfaec12087300
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-17 10:33:59 +0800

    Created Shapes Calculator

---
+++ lib/screens/root/tabs/Tools/tools/shapes_calculator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShapesCalculator extends StatelessWidget {
+  const ShapesCalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
