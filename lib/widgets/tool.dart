9580a26ec616801e59cf4c48e03327969e279c3a
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-05-26 13:36:24 +0800

    Reduced jank in moving between tabs by making widgets render with their tab data in the first place

--- lib/widgets/tool.dart
+++ lib/widgets/tool.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:mathx_android/widgets/textfieldlist.dart';
 
 class Tool extends StatefulWidget {
-  Tool({Key? key, this.displayOptions, this.innerTabs, this.tabs})
+  Tool({Key? key, this.displayOptions, this.tabs, this.builder})
       : super(key: key);
 
   late List<Widget>? displayOptions;
   late Map<String, Widget>? tabs;
-  late Map<String, Widget>? innerTabs;
+  late Widget Function(String tab)? builder;
 
   @override
   _ToolState createState() => _ToolState();
 }
 
 class _ToolState extends State<Tool> {
-  TextFieldListController controller = TextFieldListController();
-  final _formKey = GlobalKey<FormBuilderState>();
+  String activeTab = "";
+  @override
+  void initState() {
+    activeTab = widget.tabs?.keys.first ?? "";
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,49 +35,79 @@ class _ToolState extends State<Tool> {
                             child: widget.tabs![key],
                           ))
                       .toList(),
+                  onTap: (index) {
+                    setState(() {
+                      activeTab = widget.tabs!.keys.toList()[index];
+                    });
+                  },
                 ),
               ),
-              body: TabBarView(children: [
-                Column(
-                  children: [
-                    TextFieldList(
-                      limitEntries: const {"a": 1, "b": 2},
-                      controller: controller,
-                      onChange: (values) {
-                        print(values);
-                      },
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            controller.addNewField();
-                          });
-                        },
-                        child: const Icon(Icons.plus_one))
-                  ],
-                ),
-                Column(
-                  children: [
-                    TextFieldList(
-                      limitEntries: const {"a": 1, "b": 2},
-                      controller: controller,
-                      onChange: (values) {
-                        print(values);
-                      },
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            controller.addNewField();
-                          });
-                        },
-                        child: const Icon(Icons.plus_one))
-                  ],
-                ),
-              ]),
+              body: TabBarView(
+                children: List<Widget>.generate(
+                    widget.tabs!.values.length,
+                    (index) =>
+                        widget.builder
+                            ?.call(widget.tabs!.keys.toList()[index]) ??
+                        Container()),
+              ),
             ))
-        : const Scaffold(
-            body: Text("am"),
-          );
+        : widget.builder?.call(activeTab) ?? Container();
+  }
+}
+
+class NestedTabBar extends StatefulWidget {
+  const NestedTabBar(this.outerTab, {super.key});
+
+  final String outerTab;
+
+  @override
+  State<NestedTabBar> createState() => _NestedTabBarState();
+}
+
+class _NestedTabBarState extends State<NestedTabBar>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TabBar.secondary(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(text: 'Overview'),
+            Tab(text: 'Specifications'),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: <Widget>[
+              Card(
+                margin: const EdgeInsets.all(16.0),
+                child: Center(child: Text('${widget.outerTab}: Overview tab')),
+              ),
+              Card(
+                margin: const EdgeInsets.all(16.0),
+                child: Center(
+                    child: Text('${widget.outerTab}: Specifications tab')),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
