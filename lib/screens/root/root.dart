f566de6281a75ba98e04bfa68466f92845e3dedd
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-05 23:50:02 +0800

    Added abilities to minimise bottom nav bar and pass on App_links instance to tools

--- lib/screens/root/root.dart
+++ lib/screens/root/root.dart
@@ -1,3 +1,4 @@
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:mathx_android/screens/root/tabs/Cheatsheet/cheatsheet.dart';
 import 'package:mathx_android/screens/root/tabs/Notes/notes.dart';
@@ -11,26 +12,76 @@ class root extends StatefulWidget {
 }
 
 class _rootState extends State<root> {
-  List<Widget> tabs = [const NotesPage(), const CheatsheetPage(), ToolsPage()];
+  List<Widget> tabs = [];
 
   int selectedTab = 0;
+  bool hideBottomBar = false;
+  final _appLinks = AppLinks();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _appLinks.allUriLinkStream.listen((uri) {
+      print("uri: ${uri.toString().startsWith("mathx:///calculator")}");
+      if (uri.toString().startsWith("mathx:///calculator")) {
+        setState(() {
+          selectedTab = 2;
+        });
+      }
+    });
+
+    setState(() {
+      tabs = [
+        const NotesPage(),
+        const CheatsheetPage(),
+        ToolsPage(
+          hideBottomBar: (tohideBottomBar) {
+            setState(() {
+              print("a");
+              hideBottomBar = tohideBottomBar;
+            });
+          },
+          appLinkInstance: _appLinks,
+        )
+      ];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.note), label: "Notes"),
-          NavigationDestination(icon: Icon(Icons.book), label: "Cheatsheet"),
-          NavigationDestination(icon: Icon(Icons.pan_tool), label: "Tools")
-        ],
-        selectedIndex: selectedTab,
-        onDestinationSelected: (index) {
+    tabs = [
+      const NotesPage(),
+      const CheatsheetPage(),
+      ToolsPage(
+        hideBottomBar: (tohideBottomBar) {
           setState(() {
-            selectedTab = index;
+            print("a");
+            hideBottomBar = tohideBottomBar;
           });
         },
-      ),
+        appLinkInstance: _appLinks,
+      )
+    ];
+
+    return Scaffold(
+      bottomNavigationBar: !hideBottomBar
+          ? NavigationBar(
+              destinations: const [
+                NavigationDestination(icon: Icon(Icons.note), label: "Notes"),
+                NavigationDestination(
+                    icon: Icon(Icons.book), label: "Cheatsheet"),
+                NavigationDestination(
+                    icon: Icon(Icons.pan_tool), label: "Tools")
+              ],
+              selectedIndex: selectedTab,
+              onDestinationSelected: (index) {
+                setState(() {
+                  selectedTab = index;
+                });
+              },
+            )
+          : null,
       body: tabs[selectedTab],
     );
   }
