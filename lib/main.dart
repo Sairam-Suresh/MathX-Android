64b2bbb1eb8eaf5d8fdfd15e8e88b94b5dcb1ddc
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-05 12:19:33 +0800

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,8 @@ import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:mathx_android/screens/root/root.dart';
 import 'package:mathx_android/screens/welcome/welcome.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-  final database = databaseFactory.openDatabase('notes_database.db');
-
   runApp(const MyApp());
 }
 
