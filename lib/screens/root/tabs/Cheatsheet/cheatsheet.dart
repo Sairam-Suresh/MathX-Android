2d397b5d958b0f4f35d399c615793801a7ed4c60
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-15 00:29:20 +0800

    Added list of topics to cheatsheet

--- lib/screens/root/tabs/Cheatsheet/cheatsheet.dart
+++ lib/screens/root/tabs/Cheatsheet/cheatsheet.dart
@@ -1,38 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
 
-class CheatsheetDetail {
+class CheatsheetDetails {
   final String title;
-  final String notePath;
+  final String? notePath;
 
-  CheatsheetDetail(this.title, this.notePath);
+  CheatsheetDetails(this.title, this.notePath);
 }
 
 var cheatsheetsData = [
   [
-    CheatsheetDetail("sec1 topic1", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec1 topic2", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec1 topic3", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec1 topic4", "pdfs/testpdf.pdf"),
+    CheatsheetDetails("Numbers and Their Operations Part 1", ""),
+    CheatsheetDetails("Numbers and Their Operations Part 2", ""),
+    CheatsheetDetails("Percentages", ""),
+    CheatsheetDetails("Basic Algebra and Algebraic Manipulation", ""),
+    CheatsheetDetails("Linear Equations and Inequalities", ""),
+    CheatsheetDetails("Functions and Linear Graphs", ""),
+    CheatsheetDetails("Basic Geometry", ""),
+    CheatsheetDetails("Polygons", ""),
+    CheatsheetDetails("Geometrical Construction", ""),
+    CheatsheetDetails("Number Sequences", ""),
   ],
   [
-    CheatsheetDetail("sec2 topic1", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec2 topic2", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec2 topic3", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec2 topic4", "pdfs/testpdf.pdf"),
+    CheatsheetDetails("Similarity and Congruence Part 1", ""),
+    CheatsheetDetails("Similarity and Congruence Part 2", ""),
+    CheatsheetDetails("Ratio and Proportion", ""),
+    CheatsheetDetails("Direct and Inverse Proportions", ""),
+    CheatsheetDetails("Pythagoras Theorem", ""),
+    CheatsheetDetails("Trigonometric Ratios", ""),
   ],
   [
-    CheatsheetDetail("sec3 topic1", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec3 topic2", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec3 topic3", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec3 topic4", "pdfs/testpdf.pdf"),
+    CheatsheetDetails("Indices", ""),
+    CheatsheetDetails("Surds", ""),
+    CheatsheetDetails("Functions and Graphs", ""),
+    CheatsheetDetails("Quadratic Functions, Equations, and Inequalities", ""),
+    CheatsheetDetails("Coordinate Geometry", ""),
+    CheatsheetDetails("Exponentials and Logarithms", ""),
+    CheatsheetDetails("Further Coordinate Geometry", ""),
+    CheatsheetDetails("Linear Law", ""),
+    CheatsheetDetails("Geometrical Properties of Circles", ""),
+    CheatsheetDetails("Polynomials and Partial Fractions", "")
   ],
-  [
-    CheatsheetDetail("sec4 topic1", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec4 topic2", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec4 topic3", "pdfs/testpdf.pdf"),
-    CheatsheetDetail("sec4 topic4", "pdfs/testpdf.pdf"),
-  ],
+  [CheatsheetDetails("Coming Soon...", null)],
 ];
 
 class CheatsheetPage extends StatelessWidget {
@@ -46,99 +54,36 @@ class CheatsheetPage extends StatelessWidget {
       ),
       body: ListView.builder(
           itemCount: 4,
+          shrinkWrap: true,
           itemBuilder: (BuildContext buildContext, int index) {
-            return ListTile(
+            return ExpansionTile(
               title: Text("Secondary ${index + 1}"),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => LevelCheatsheet(level: index)));
-              },
+              children: [
+                ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: cheatsheetsData[index].length,
+                  itemBuilder: (context, innerIndex) {
+                    return ListTile(
+                      title: Text(
+                        cheatsheetsData[index][innerIndex].title,
+                      ),
+                      trailing:
+                          cheatsheetsData[index][innerIndex].notePath != null
+                              ? Icon(Icons.chevron_right)
+                              : null,
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Divider(),
+                    );
+                  },
+                )
+              ],
             );
           }),
-    );
-  }
-}
-
-class LevelCheatsheet extends StatelessWidget {
-  const LevelCheatsheet({super.key, required this.level});
-
-  final int level;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Secondary ${level + 1}")),
-      body: ListView.builder(
-          itemCount: cheatsheetsData[level].length,
-          itemBuilder: (BuildContext buildContext, int index) {
-            return ListTile(
-              title: Text(cheatsheetsData[level][index].title),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          MyPdfViewer(pdfPath: 'assets/my_document.pdf')),
-                );
-              },
-            );
-          }),
-    );
-  }
-}
-
-// class CheatsheetDetailedView extends StatelessWidget {
-//   const CheatsheetDetailedView(
-//       {super.key, required this.level, required this.noteIndex});
-
-//   final int level;
-//   final int noteIndex;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text(cheatsheetsData[level][noteIndex].title)),
-//       body:
-//     );
-//   }
-// }
-
-class MyPdfViewer extends StatefulWidget {
-  final String pdfPath;
-  MyPdfViewer({required this.pdfPath});
-  @override
-  _MyPdfViewerState createState() => _MyPdfViewerState();
-}
-
-class _MyPdfViewerState extends State<MyPdfViewer> {
-  late PDFViewController pdfViewController;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("My PDF Document"),
-      ),
-      body: PDFView(
-        filePath: widget.pdfPath,
-        autoSpacing: true,
-        enableSwipe: true,
-        pageSnap: true,
-        swipeHorizontal: true,
-        onError: (error) {
-          debugPrint(error);
-        },
-        onPageError: (page, error) {
-          debugPrint('$page: ${error.toString()}');
-        },
-        onViewCreated: (PDFViewController vc) {
-          pdfViewController = vc;
-        },
-        onPageChanged: (int? page, int? total) {
-          debugPrint('page change: $page/$total');
-        },
-      ),
     );
   }
 }
