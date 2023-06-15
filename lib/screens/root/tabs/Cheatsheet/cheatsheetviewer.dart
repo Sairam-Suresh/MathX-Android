de517af47b549d825d74a4dc2ed8a1674505100d
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-15 22:22:28 +0800

    Trying out flutter_hooks and using it on cheatsheetviewer first

--- lib/screens/root/tabs/Cheatsheet/cheatsheetviewer.dart
+++ lib/screens/root/tabs/Cheatsheet/cheatsheetviewer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class CheatSheetViewer extends StatelessWidget {
+class CheatSheetViewer extends HookWidget {
   const CheatSheetViewer({super.key});
 
   @override
