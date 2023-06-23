07a340a85ddbdff9e2e1fb04101962f9daa51954
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-23 11:24:44 +0800

    Added URL Launcher when a link is clicked on in markdown

--- lib/widgets/custommarkdownbody.dart
+++ lib/widgets/custommarkdownbody.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> _launchUrl(Uri url) async {
+  if (!await launchUrl(url)) {
+    throw Exception('Could not launch $url');
+  }
+}
 
 MarkdownBody buildMarkdownBody(String text, bool isDarkMode) {
   return MarkdownBody(
     data: text,
     shrinkWrap: true,
-    onTapLink: (text, link, _) {},
+    onTapLink: (text, link, _) {
+      if ((link ?? "") != "") {
+        _launchUrl(Uri.parse(link!));
+      }
+    },
     styleSheet: MarkdownStyleSheet(
       blockquoteDecoration: BoxDecoration(
         border: Border(
