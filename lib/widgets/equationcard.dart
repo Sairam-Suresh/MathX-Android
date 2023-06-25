76abff68ff704e9eb8e10852616dfefdf5a522c4
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-25 09:54:59 +0800

    Added LabelText parameter to tell users what the formula calculates

--- lib/widgets/equationcard.dart
+++ lib/widgets/equationcard.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_math_fork/flutter_math.dart';
 
 class EquationCard extends StatelessWidget {
-  const EquationCard({super.key, required this.text});
+  const EquationCard({super.key, required this.text, this.labelText});
 
   final String text;
+  final String? labelText;
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +14,33 @@ class EquationCard extends StatelessWidget {
           padding: const EdgeInsets.all(10),
           child: SizedBox(
               width: MediaQuery.of(context).size.width,
-              child: Center(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Math.tex(
-                    text,
-                    textStyle: const TextStyle(fontSize: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (labelText != null)
+                    Text(
+                      labelText!,
+                      style: const TextStyle(fontSize: 10),
+                    ),
+                  if (labelText != null)
+                    const Divider(
+                      height: 5,
+                    ),
+                  if (labelText != null)
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  Center(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Math.tex(
+                        text,
+                        textStyle: const TextStyle(fontSize: 20),
+                      ),
+                    ),
                   ),
-                ),
+                ],
               ))),
     );
   }
