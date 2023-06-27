1ca16aa6240eade75a276a6a75b5ceb70f9c3e41
sairam-suresh <85359258+Sairam-Suresh@users.noreply.github.com>
2023-06-27 11:48:45 +0800

    Added animation when text fields are added in

--- lib/widgets/dynamictextfieldlist.dart
+++ lib/widgets/dynamictextfieldlist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
 class DynamicTextFieldList extends StatefulWidget {
@@ -44,41 +45,45 @@ class _DynamicTextFieldListState extends State<DynamicTextFieldList> {
             controller.text = _values[index] ?? "";
             controller.selection = TextSelection.fromPosition(
                 TextPosition(offset: controller.text.length));
-
-            return TextFormField(
-              validator: widget.validators ??
-                  FormBuilderValidators.compose([
-                    FormBuilderValidators.numeric(),
-                    FormBuilderValidators.required()
-                  ]),
-              keyboardType: TextInputType.number,
-              controller: controller,
-              onChanged: (value) {
-                setState(() {
-                  _values[index] = value;
-                  widget.onChange?.call(_values,
-                      _formKey.currentState?.validate() ?? false, widget.count);
-                });
-              },
-              decoration: InputDecoration(
-                  labelText: widget.nameBuilder == null
-                      ? "Number ${index + 1}"
-                      : widget.nameBuilder?.call(_values.length, index),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _values.removeAt(index);
-                        widget.onChange?.call(
-                            _values,
-                            _formKey.currentState?.validate() ?? false,
-                            widget.count - 1);
-                      });
-                    },
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
-                  )),
+            return Animate(
+              effects: [SlideEffect(duration: 100.milliseconds)],
+              child: TextFormField(
+                validator: widget.validators ??
+                    FormBuilderValidators.compose([
+                      FormBuilderValidators.numeric(),
+                      FormBuilderValidators.required()
+                    ]),
+                keyboardType: TextInputType.number,
+                controller: controller,
+                onChanged: (value) {
+                  setState(() {
+                    _values[index] = value;
+                    widget.onChange?.call(
+                        _values,
+                        _formKey.currentState?.validate() ?? false,
+                        widget.count);
+                  });
+                },
+                decoration: InputDecoration(
+                    labelText: widget.nameBuilder == null
+                        ? "Number ${index + 1}"
+                        : widget.nameBuilder?.call(_values.length, index),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _values.removeAt(index);
+                          widget.onChange?.call(
+                              _values,
+                              _formKey.currentState?.validate() ?? false,
+                              widget.count - 1);
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                    )),
+              ),
             );
           }),
     );
